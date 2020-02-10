9fb5b22b69867d0340913d1f51589d8912ce5a50
Grzegorz Palian <greekomp@gmail.com>
2020-02-10 22:34:17 +0100

    Added StringParsingExtensions for parsing string into numbers regardless of cultures decimal separator.

---
+++ Assets/AthanorUtils/Runtime/Utilities/Conversion Utilities/StringParsingExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+namespace Athanor
+{
+	public static class StringParsingExtensions
+	{
+		public static bool TryParseFloatMulticultural(this string value, out float result)
+		{
+			if (!float.TryParse(value, NumberStyles.Any, CultureInfo.CurrentCulture, out result) &&
+				!float.TryParse(value, NumberStyles.Any, CultureInfo.GetCultureInfo("en-US"), out result) &&
+				!float.TryParse(value, NumberStyles.Any, CultureInfo.InvariantCulture, out result))
+			{
+				result = default;
+				return false;
+			}
+
+			return true;
+		}
+		public static bool TryParseIntMulticultural(this string value, out int result)
+		{
+			if (!int.TryParse(value, NumberStyles.Any, CultureInfo.CurrentCulture, out result) &&
+				!int.TryParse(value, NumberStyles.Any, CultureInfo.GetCultureInfo("en-US"), out result) &&
+				!int.TryParse(value, NumberStyles.Any, CultureInfo.InvariantCulture, out result))
+			{
+				result = default;
+				return false;
+			}
+
+			return true;
+		}
+		public static bool TryParseDoubleMulticultural(this string value, out double result)
+		{
+			if (!double.TryParse(value, NumberStyles.Any, CultureInfo.CurrentCulture, out result) &&
+				!double.TryParse(value, NumberStyles.Any, CultureInfo.GetCultureInfo("en-US"), out result) &&
+				!double.TryParse(value, NumberStyles.Any, CultureInfo.InvariantCulture, out result))
+			{
+				result = default;
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
