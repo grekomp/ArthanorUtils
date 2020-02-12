51b3c5a891dc02d0057b309544f9757bf249ded4
Grzegorz Palian <greekomp@gmail.com>
2020-02-12 23:22:33 +0100

    Removed messages list from Log, a more robust solution for viewing past messages need to be developed.

--- Runtime/Log/Log.cs
+++ Runtime/Log/Log.cs
@@ -21,13 +21,9 @@ namespace Athanor
 	{
 		public readonly static string defaultDebugTag = "QuickDebug";
 
-		public static List<string> messages = new List<string>();
-		public static event Action OnMessagesChanged;
-
 		public static void PrintLog(LogLevel logLevel, object tag, object message, UnityEngine.Object context = null)
 		{
 			string logString = $"[{logLevel.ToString().Substring(0, 3)}] {tag}: {message}";
-			messages.Add(logString);
 
 			if (context == null)
 			{
@@ -65,8 +61,6 @@ namespace Athanor
 						break;
 				}
 			}
-
-			OnMessagesChanged?.SafeInvoke();
 		}
 
 		public static void D(object message)
