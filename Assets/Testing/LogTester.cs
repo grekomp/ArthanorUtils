9e7fcd499dfc7b593d1dd518e81b7ea8fabbbead
Grzegorz Palian <greekomp@gmail.com>
2020-02-12 23:20:40 +0100

    Added custom Log class

---
+++ Assets/Testing/LogTester.cs
@@ -0,0 +1,13 @@
+﻿using Athanor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogTester : MonoBehaviour
+{
+	[ContextMenu("Test")]
+	public void TestLog()
+	{
+		Log.D(this, "This is a message", this);
+	}
+}
