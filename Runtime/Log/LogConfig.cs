6baf9768fcbb55ddfca740356b63a132e013bb35
Grzegorz Palian <greekomp@gmail.com>
2020-02-18 18:59:27 +0100

    Added LogConfig for configurable log filtering. Added ScriptableObjectSingleton for automated creation of ScriptableObject config files in Resources.

---
+++ Runtime/Log/LogConfig.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Athanor
+{
+	[CreateAssetMenu(menuName = "Config/Log Config")]
+	public class LogConfig : ScriptableObjectSingleton<LogConfig>
+	{
+		[Serializable]
+		public class LogConfigLine
+		{
+			public string LogTag;
+			public LogLevel LogLevelsEditor = (LogLevel)int.MaxValue;
+			public LogLevel LogLevelsBuild = (LogLevel)int.MaxValue;
+		}
+
+		[Header("Log Settings")]
+		public bool overrideLogAll = false;
+		[SerializeField] protected List<LogConfigLine> Filters = new List<LogConfigLine>();
+
+
+		#region Public methods
+		public bool ShouldLogMessage(string logTag, LogLevel logLevel)
+		{
+			if (overrideLogAll) return true;
+
+			LogConfigLine line = Filters.Find(l => l.LogTag == logTag);
+
+			if (line == null)
+			{
+				line = new LogConfigLine() { LogTag = logTag };
+				Filters.Add(line);
+			}
+
+			if (Application.isEditor)
+			{
+				return (line.LogLevelsEditor & logLevel) > 0;
+			}
+			else
+			{
+				return (line.LogLevelsBuild & logLevel) > 0;
+			}
+		}
+		#endregion
+	}
+}
---
+++ Runtime/Utilities/ScriptableObjectSingleton/ScriptableObjectSingleton.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+
+namespace Athanor
+{
+	public class ScriptableObjectSingleton<T> : ScriptableObject where T : ScriptableObject
+	{
+		#region Singleton
+		protected static T _instance;
+		public static T Instance {
+			get {
+				Initialize();
+
+				return _instance;
+			}
+		}
+
+		//#if UNITY_EDITOR
+		//		[InitializeOnLoadMethod]
+		//#else
+		//	[RuntimeInitializeOnLoadMethod]
+		//#endif
+		protected static void Initialize()
+		{
+			if (_instance == null)
+			{
+				var found = Resources.LoadAll<T>("");
+				_instance = found.FirstOrDefault();
+
+				if (_instance == null)
+				{
+					_instance = CreateNewInstance();
+					Log.Warning($"ScriptableObjectSingleton<{typeof(T)}>", $"Cannot find instance of ScriptableObjectSingleton<{typeof(T)}>, creating a new config file: Assets/Resources/{typeof(T).Name}.asset", _instance);
+				}
+			}
+		}
+
+		private static T CreateNewInstance()
+		{
+			ScriptableObject newObject = CreateInstance<T>();
+#if UNITY_EDITOR
+			if (AssetDatabase.IsValidFolder("Assets/Resources") == false)
+			{
+				AssetDatabase.CreateFolder("Assets", "Resources");
+			}
+
+			AssetDatabase.CreateAsset(newObject, $"Assets/Resources/{typeof(T).Name}.asset");
+			AssetDatabase.SaveAssets();
+#endif
+			return newObject as T;
+		}
+		#endregion
+	}
+}
