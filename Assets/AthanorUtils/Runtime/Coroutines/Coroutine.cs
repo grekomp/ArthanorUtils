c544e9a83c279dfff0ba5ee6bdeb55c7073f67f4
Grzegorz Palian <greekomp@gmail.com>
2020-02-26 08:33:43 +0100

    [WIP] Added custom coroutine wrapper

---
+++ Assets/AthanorUtils/Runtime/Coroutines/Coroutine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Athanor
+{
+	namespace Athanor
+	{
+		public class Coroutine<T>
+		{
+			private IEnumerator _target;
+			public T result;
+			public Coroutine coroutine { get; private set; }
+
+			public Coroutine(MonoBehaviour owner_, IEnumerator target_)
+			{
+				_target = target_;
+				coroutine = owner_.StartCoroutine(Run());
+			}
+
+			private IEnumerator Run()
+			{
+				while (_target.MoveNext())
+				{
+					result = (T)_target.Current;
+					yield return result;
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Tests/Runtime/CoroutineTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+	public class CoroutineTests
+	{
+		GameObject coroutineRunner;
+
+		[SetUp]
+		public void SetUp()
+		{
+			coroutineRunner = new GameObject("Test coroutine runner");
+		}
+
+		[UnityTest]
+		public IEnumerator CoroutineT_Should_ReturnResultAfterCoroutineHasFinishedRunning()
+		{
+
+
+			yield return new WaitForSeconds(1);
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			GameObject.Destroy(coroutineRunner);
+		}
+	}
+}
